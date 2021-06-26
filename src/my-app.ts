da2e1ce74ee9e4adc1e1a3adc6843d1d7c8db3ec
Dwayne <dwaynecharrington@gmail.com>
2021-06-26 23:54:02 +1000

    feat(app): price poll

--- src/my-app.ts
+++ src/my-app.ts
@@ -11,5 +11,13 @@ export class MyApp {
 
   async binding(): Promise<void> {
     this.prices = await this.api.getPrices(config.coins);
+
+    this.priceUpdate();
+  }
+
+  priceUpdate(): void {
+    setInterval(async () => {
+      this.prices = await this.api.getPrices(config.coins);
+    }, 1000);
   }
 }
